c2e9ed8fe4e115c1939cd5b1e85a876dad03d216
nattsp <drnphillips@gmail.com>
2015-05-10 20:07:21 +1000

    Graph in png file

--- plot1.R
+++ plot1.R
@@ -14,13 +14,18 @@ dataAll$Time <- strptime(paste(dataAll$Date, dataAll$Time), "%Y-%m-%d %H:%M:%S")
 # Subset data to the two days in question in the assignment.
 data <- dataAll[(dataAll$Date == "2007-02-01" | dataAll$Date == "2007-02-02"), ]
 
+#Save this subsetted data for later exploratory graphs
+write.table(data, file = "data.txt", row.name=FALSE)
 
 
-# Plot 1
+
+# Plot 1 Check the graph on the screen
 hist(data$Global_active_power, col = "red", main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)")
 
 # Save Plot 1 to file
 png(file = "plot1.png",
     width = 480, height = 480) ## Open PNG device; create 'plot1.png' in my working directory
+hist(data$Global_active_power, col = "red", main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)") #Draw the graph in the png file
 dev.off()  # Close the png device
